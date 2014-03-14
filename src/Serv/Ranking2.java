e64c59eb4c4191b7500d4eeaa2bc23424aed0dc3
matsui <win.matsui@gmail.com>
2014-03-14 10:49:39 +0900

    生徒成績表示　ランキング以外

--- src/Serv/Ranking2.java
+++ src/Serv/Ranking2.java
@@ -36,12 +36,30 @@ public class Ranking2 extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		if (request.getParameter("sid") != null){
+			int sid = Integer.parseInt(request.getParameter("sid"));
+
+			SeisekiKanriDB db1 = new SeisekiKanriDB();
+			ArrayList<TokutenTbl2> list1 = db1.getTokutenTbl2List(sid);
+			ArrayList<TokutenTbl2> list2 = db1.getTokutenTbl2List(sid,0);
+			db1.close();
+			request.setAttribute("sid", Integer.toString(sid));
+			request.setAttribute("slist1", list1);
+			request.setAttribute("slist2", list2);
+			request.setAttribute("mode","1");
+
+		}else {
+			request.setAttribute("mode","0");
+		}
 		int nen = Integer.parseInt(request.getParameter("nen"));
 		String kyu=request.getParameter("kyu");
 
 		SeisekiKanriDB db = new SeisekiKanriDB();
 		ArrayList<SeitoAll> list = db.getSeitoAllList(nen,kyu);
 		db.close();
+
+
 
 		request.setAttribute("slist", list);
 		request.setAttribute("nen", Integer.toString(nen));
