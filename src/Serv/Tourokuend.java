08e2a68d381a34de5094c6cf097013318b8924ff
saitou <tsukasa.saitou.si1@gmail.com>
2014-03-14 09:25:10 +0900

    例外処理とエラー画面を作成

--- src/Serv/Tourokuend.java
+++ src/Serv/Tourokuend.java
@@ -49,24 +49,46 @@ public class Tourokuend extends HttpServlet {
 		ArrayList<TestSyousai> tlist = new ArrayList<TestSyousai>();
 
 		// 処理
-		for(int i=0;i<num;i++) {
-			str_sid = "sid" + i;
-			str_ten = "ten" + i;
-			tlist = db.getTestSyousaiList(tid,kid);
+		try {
+			for(int i=0;i<num;i++) {
+				str_ten = "ten" + i;
+				if(Integer.parseInt(request.getParameter(str_ten)) < 0 || Integer.parseInt(request.getParameter(str_ten)) > 100){
+					db.close();
 
-			if(!tlist.isEmpty()){
-				tenlist = db.getTokutenList(Integer.parseInt(request.getParameter(str_sid)),tlist.get(0).getTdid());
-				java.lang.System.out.println(request.getParameter(str_sid));
-				java.lang.System.out.println(tlist.get(0).getTdid());
-				java.lang.System.out.println(request.getParameter(str_ten));
-				if(!tenlist.isEmpty()){
-					db.updateTokuten(tenlist.get(0).getTenid(), Integer.parseInt(request.getParameter(str_ten)));
-				}
-				else{
-					db.insertTokuten(Integer.parseInt(request.getParameter(str_sid)), tlist.get(0).getTdid(), Integer.parseInt(request.getParameter(str_ten)));
+					request.setAttribute("outstr", "得点は0～100で入力してください");
+					// 処理を"test.jsp"に処理を受け渡す
+					RequestDispatcher dispatch = request.getRequestDispatcher("error.jsp");
+					dispatch.forward(request, response);
+					return;
 				}
 			}
 
+			for(int i=0;i<num;i++) {
+				str_sid = "sid" + i;
+				str_ten = "ten" + i;
+				tlist = db.getTestSyousaiList(tid,kid);
+
+				if(!tlist.isEmpty()){
+						tenlist = db.getTokutenList(Integer.parseInt(request.getParameter(str_sid)),tlist.get(0).getTdid());
+
+						if(!tenlist.isEmpty()){
+							db.updateTokuten(tenlist.get(0).getTenid(), Integer.parseInt(request.getParameter(str_ten)));
+						}
+						else{
+							db.insertTokuten(Integer.parseInt(request.getParameter(str_sid)), tlist.get(0).getTdid(), Integer.parseInt(request.getParameter(str_ten)));
+						}
+				}
+
+			}
+		} catch (NumberFormatException e) {
+			// TODO 自動生成された catch ブロック
+			db.close();
+
+			request.setAttribute("outstr", "不正な入力がありました");
+			// 処理を"test.jsp"に処理を受け渡す
+			RequestDispatcher dispatch = request.getRequestDispatcher("error.jsp");
+			dispatch.forward(request, response);
+			return;
 		}
 		db.close();
 
